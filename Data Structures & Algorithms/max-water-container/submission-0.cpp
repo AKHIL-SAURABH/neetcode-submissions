1a3e671cdb9454de571b5a20d84a435c6122e69a
AKHIL SAURABH <akhilsaurabh65@gmail.com>
2026-05-27 08:48:12 +0530

    Add: max-water-container - submission-0

---
+++ Data Structures & Algorithms/max-water-container/submission-0.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int maxArea(vector<int>& heights) {
+        int n = heights.size();
+
+        int left = 0;
+        int right = n-1;
+        int max_area = 0;
+
+        while(left < right){
+
+            // width = (right - left)
+            // lenght = min(height[left] , height[right]) because till the smaller size poll the water will fill
+            // that is why the curr_area will be (right - left) * min(heights[left] , heights[right]
+            max_area = max(max_area, (right - left) * min(heights[left] , heights[right]));
+
+            if(heights[left] < heights[right]){
+                left++;
+            }else{
+                right--;
+            }
+        }
+        return max_area;
+    }
+};
